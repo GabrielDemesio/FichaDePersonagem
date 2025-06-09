fa5b739152584ef05ca80ffa214b99be6f4dea17
GabrielDemesio <cfmoneygamer@gmail.com>
2025-06-09 11:16:19 -0300

    implementando correções na criação de personagem

--- src/main/java/com/rpg/rpg_ficha/controllers/CharacterController.java
+++ src/main/java/com/rpg/rpg_ficha/controllers/CharacterController.java
@@ -3,8 +3,9 @@ package com.rpg.rpg_ficha.controllers;
 import com.rpg.rpg_ficha.models.CharacterModel;
 import com.rpg.rpg_ficha.service.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -22,13 +23,20 @@ public class CharacterController {
 
     @GetMapping("/{id}")
     public CharacterModel findById(@PathVariable Long id){
-        return characterService.findById(id).get();
+        return characterService.findById(id);
     }
     @DeleteMapping("/{id}")
     public void deleteCharacter(@PathVariable Long id){
         characterService.deleteCharacter(id);
     }
 
-
-
+    @PostMapping
+    public CharacterModel saveCharacter(@Valid @RequestBody CharacterModel characterModel){
+        return characterService.saveCharacter(characterModel);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<CharacterModel> updateCharacter(@PathVariable Long id, @RequestBody CharacterModel characterDetails) {
+        CharacterModel updatedCharacter = characterService.updateCharacter(id, characterDetails);
+        return ResponseEntity.ok(updatedCharacter);
+    }
 }
